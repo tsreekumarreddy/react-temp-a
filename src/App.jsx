a5447dea53118a5cb331bd91f1868761b1565673
tsreekumarreddy <138125465+tsreekumarreddy@users.noreply.github.com>
2025-03-27 11:36:29 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import App6 from "./components/App6";
 import App7 from "./components/App7";
 import App8 from "./components/App8";
 import App9 from "./components/App9";
+import App10 from "./components/App10";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
       <Link to="app7">App7</Link>
       <Link to="app8">App8</Link>
       <Link to="app9">App9</Link>
+      <Link to="app10">App10</Link>
     </div>
     <hr></hr>
     <div>
@@ -40,6 +42,7 @@ function App() {
         <Route path="app7" element={<App7/>}></Route>
         <Route path="app8" element={<App8/>}></Route>
         <Route path="app9" element={<App9/>}></Route>
+        <Route path="app10" element={<App10/>}></Route>
       </Routes>
     </div>
     </BrowserRouter>
