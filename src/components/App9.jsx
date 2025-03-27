c1d9b20f3533db9892df11184003ae23236f1d0f
tsreekumarreddy <138125465+tsreekumarreddy@users.noreply.github.com>
2025-03-27 10:04:43 +0530

    new

--- src/components/App9.jsx
+++ src/components/App9.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { useState,useRef } from "react";
 export default function App9(){
+    const[color,setColor]=useState();
+    const txtRef =useRef();
+    const handleSubmit=()=>{
+        txtRef.current.style.color=color
+    }
     return (
         <div>
         <h1>This is App9</h1>
+        <input type="text" onChange={(e)=>setColor(e.target.value)} />
+        <button onClick={handleSubmit}>Submit</button>
+        <h2 ref={txtRef}>HELLO</h2>
         </div>
     )
 }
